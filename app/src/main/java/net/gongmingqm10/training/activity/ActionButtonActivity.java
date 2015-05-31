c702aefe038a9aadbd7c1f73ffb0bc30fadbaa63
gongming <gongmingqm10@foxmail.com>
2015-05-31 12:20:11 +0800

    Add Collapse search button and ShareProvider in ActionButtonActivity

--- app/src/main/java/net/gongmingqm10/training/activity/ActionButtonActivity.java
+++ app/src/main/java/net/gongmingqm10/training/activity/ActionButtonActivity.java
@@ -2,8 +2,11 @@ package net.gongmingqm10.training.activity;
 
 import android.content.Intent;
 import android.provider.Settings;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -21,7 +24,35 @@ public class ActionButtonActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_action_button, menu);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menu.findItem(R.id.action_share));
+
+        setUpSearchView(searchView);
+        setUpShareProvider(shareActionProvider);
         return true;
+    }
+
+    private void setUpShareProvider(ShareActionProvider shareActionProvider) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/*");
+
+        shareActionProvider.setShareIntent(intent);
+    }
+
+    private void setUpSearchView(SearchView searchView) {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(ActionButtonActivity.this, "Text submit event: " + query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(ActionButtonActivity.this, "Text change event: " + newText, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 
     @Override
@@ -30,9 +61,6 @@ public class ActionButtonActivity extends AppCompatActivity {
             case R.id.action_settings:
                 openSettings();
                 return true;
-            case R.id.action_search:
-                openSearch();
-                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -40,10 +68,6 @@ public class ActionButtonActivity extends AppCompatActivity {
 
     private void openSettings() {
         startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
-    }
-
-    private void openSearch() {
-        Toast.makeText(this, "Searching...", Toast.LENGTH_SHORT).show();
     }
 
 }

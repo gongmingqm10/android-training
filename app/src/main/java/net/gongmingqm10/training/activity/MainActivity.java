84a6ad654aea613cc3d041e073f9c5668942b2fc
gongming <gongmingqm10@foxmail.com>
2015-05-31 11:11:31 +0800

    Enable Action Bar theme

---
+++ app/src/main/java/net/gongmingqm10/training/activity/MainActivity.java
@@ -0,0 +1,47 @@
+package net.gongmingqm10.training.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import net.gongmingqm10.training.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnItemClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.list)
+    protected ListView featureList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this, this);
+        inflateListView();
+    }
+
+    private void inflateListView() {
+        String[] texts = new String[]{
+                "ActionBar Buttons"
+        };
+        featureList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, texts));
+
+    }
+
+    @OnItemClick(R.id.list)
+    protected void openPage(int position) {
+        switch(position) {
+            case 0:
+                startActivity(new Intent(this, ActionButtonActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
+
+}

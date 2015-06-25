0a3bb0ca9d3646b7836041ab4d76bb0af9311831
gongming <gongmingqm10@foxmail.com>
2015-06-25 09:24:55 +0800

    Add navigation view for main activity

--- app/src/main/java/net/gongmingqm10/training/activity/MainActivity.java
+++ app/src/main/java/net/gongmingqm10/training/activity/MainActivity.java
@@ -2,6 +2,8 @@ package net.gongmingqm10.training.activity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,8 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import net.gongmingqm10.training.R;
 import net.gongmingqm10.training.fragment.MainFragment;
@@ -20,15 +20,15 @@ import net.gongmingqm10.training.fragment.TabCFragment;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import butterknife.OnItemClick;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @InjectView(R.id.drawer_layout)
     protected DrawerLayout drawerLayout;
 
-    @InjectView(R.id.left_drawer)
-    protected ListView drawerList;
+    @InjectView(R.id.navigation_view)
+    protected NavigationView navigationView;
 
     @InjectView(R.id.tool_bar)
     protected Toolbar toolBar;
@@ -68,7 +68,27 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.setDrawerListener(drawerToggle);
 
         drawerTitle = new String[]{"Main Fragment", "Tab A Fragment", "Tab B Fragment", "Tab C Fragment"};
-        drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, drawerTitle));
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                switch (menuItem.getItemId()) {
+                    case R.id.main_fragment_item:
+                        drawerItemClicked(0);
+                        return true;
+                    case R.id.a_fragment_item:
+                        drawerItemClicked(1);
+                        return true;
+                    case R.id.b_fragment_item:
+                        drawerItemClicked(2);
+                        return true;
+                    case R.id.c_fragment_item:
+                        drawerItemClicked(3);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -89,12 +109,21 @@ public class MainActivity extends AppCompatActivity {
         return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
-    @OnItemClick(R.id.left_drawer)
-    protected void leftDrawerClick(int position) {
-        updateFragment(position);
+    @OnClick(R.id.floating_action_btn)
+    protected void clickFAB(View view) {
+        Snackbar.make(findViewById(R.id.content_view), "This is the snack bar", Snackbar.LENGTH_SHORT)
+                .setAction("Dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-        drawerList.setItemChecked(position, true);
-        drawerLayout.closeDrawer(drawerList);
+                    }
+                })
+                .show();
+    }
+
+    private void drawerItemClicked(int position) {
+        updateFragment(position);
+        drawerLayout.closeDrawer(navigationView);
     }
 
     private void updateFragment(int position) {
